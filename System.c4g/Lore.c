28845411f31b07e856df5bc632e4da634a01119a
Luchs <Luchs@euirc.eu>
2009-04-10 01:48:52 +0200

    * Anti-Rausnehmen der Lore ohne Effekt

--- System.c4g/Lore.c
+++ System.c4g/Lore.c
@@ -26,22 +26,14 @@ private func MaxContents() { return 100; }
 
 /* Anti-Rausnehmen */
 
-protected func Grabbed(object pClonk, bool fGrab) {
-	if(iTeam && GetPlayerTeam(pClonk -> GetOwner()) != iTeam) {
-		if(fGrab)
-			AddEffect("IntGetCheck", pClonk, 100, 1, this, GetID());
-		else
-			RemoveEffect("IntGetCheck", pClonk);
-	}
-}
-
-protected func FxIntGetCheckTimer(object pTarget, int iEffectNumber) {
-	if(pTarget -> GetAction() != "Push" || pTarget -> GetActionTarget() != this)
-		return -1;
-	var idMenu = pTarget -> GetMenu();
-	if(idMenu == 0013 || idMenu == 0018) {
-		CreateObject(TIM1) -> Explode(RandomX(20, 30));
-		pTarget -> CloseMenu();
-		return -1;
+protected func RejectContents() {
+	if(!iTeam)
+		return;
+	var aClonks = FindObjects(Find_Action("Push"), Find_ActionTarget(this));
+	for(var pClonk in aClonks) {
+		if(GetPlayerTeam(pClonk -> GetOwner()) != iTeam && (GetLength(aClonks) == 1 || (pClonk -> GetMenu() == 0013 || pClonk -> GetMenu() == 0018))) {
+			CreateObject(TIM1) -> Explode(RandomX(20, 30));
+			return 1;
+		}
 	}
 }
