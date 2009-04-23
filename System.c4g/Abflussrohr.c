be650e356f83dcdd87f85548bf734624db4d8953
Luchs <Luchs@euirc.eu>
2009-04-23 21:23:28 +0200

    * Schnelleres pumpen

---
+++ System.c4g/Abflussrohr.c
@@ -0,0 +1,33 @@
+/* Abflussrohr */
+
+#strict
+
+#appendto DPIP
+
+private func Transfer() // Abflussrohr führt zur Zeit keine Prüfung von AcceptTransfer beim Zielobjekt durch...
+{
+  var i;
+  // Angeschlossene Objekte ermitteln
+  var pPumpSource = GetActionTarget(0);
+  var pPumpTarget = GetActionTarget(1);
+  // Kein Quellobjekt
+  if (!pPumpSource) return(0);
+  // Quellobjekt hat nicht genug Energie
+  if (!EnergyCheck(1000, pPumpSource)) return(0);
+  // Zielobjekt vorhanden
+  if (pPumpTarget)
+  {
+    // Materialtransfer
+    for (i = 0; i < 200; i++)  
+      if(!ObjectInsertMaterial(ObjectExtractLiquid(pPumpSource), pPumpTarget))
+      	break;
+  }
+  // Kein Zielobjekt vorhanden
+  else
+  {
+    // Materialtransfer
+    for (i = 0; i < 200; i++)    
+      if(!InsertMaterial(ObjectExtractLiquid(pPumpSource), GetVertex(50,0), GetVertex(50,1)))
+      	break;
+  }
+}
---
+++ System.c4g/Zuflussrohr.c
@@ -0,0 +1,28 @@
+/* Zuflussrohr */
+
+#strict
+
+#appendto SPIP
+
+protected func Transfer()
+{
+  // Zielobjekt suchen
+  var pPumpTarget = GetActionTarget(0);
+  // Kein Zielobjekt  
+  if (!pPumpTarget) return(0);
+  // Zielobjekt akzeptiert keinen Transfer
+  if (!pPumpTarget->~AcceptTransfer()) return(0);
+  // Zielobjekt hat nicht genug Energie (könnte man auch in AcceptTransfer verlegen...)
+  if (!EnergyCheck(1000,pPumpTarget)) return(0);
+  // Materialtransfer
+  for (var i = 0; i < 200; i++)
+  {
+    var mat = ObjectExtractLiquid(GetActionTarget(1));
+    if (mat == -1) mat = ExtractLiquid(GetVertex(50, 0), GetVertex(50, 1));
+    if(mat == -1)
+    	break;
+    ObjectInsertMaterial(mat, pPumpTarget);
+  }
+  // Blubbern  
+  if (!Random(5)) Bubble(GetVertex(50,0), GetVertex(50,1));
+}
